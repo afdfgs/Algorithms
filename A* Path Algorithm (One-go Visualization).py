163f8d71a4f6365d41463392e21934d538bb2202
afdfgs <65339115+afdfgs@users.noreply.github.com>
2026-03-31 16:31:53 -0500

    Add docstrings to functions in A* Path Algorithm
    Added docstrings to functions for better clarity.

--- A* Path Algorithm (One-go Visualization).py
+++ A* Path Algorithm (One-go Visualization).py
@@ -1,6 +1,11 @@
 import random as rand
 
 def get_neighbors(point, grid):
+    '''
+    Purpose: Gets the neighbors around (point) and checks if it is out of bound or if it is a wall
+    input: (list) point, (list) grid
+    output: (list) neighbor
+    '''
     x,y = point
     moves = [(1,0), (-1,0), (0,1), (0,-1)]
     neighbor = []
@@ -14,9 +19,19 @@ def get_neighbors(point, grid):
     return neighbor
 
 def heuristic(current, end):
+    '''
+    Purpose: Use the Manhattan distance to calculate the distance from the current point to the ending point
+    input: (list) current, (list) end
+    output: (int) manhattan distance
+    '''
     return abs(current[0] - end[0]) + abs(current[1] - end[1])
 
 def reconstruct_path(came_from, start, end):
+    '''
+    Purpose: Constructs the entire path by starting from the ending point and going backwards, with the dictionary came_from that gives the recent point
+    input: (dict) came_from, (list) start, (list) end
+    output: (list) path
+    '''
     current = end
     path = []
 
@@ -30,6 +45,11 @@ def reconstruct_path(came_from, start, end):
     return path
 
 def get_lowest_f(open_list, g, end):
+    '''
+    Purpose: From all the neighboring points of the current list stored in open_list, checks for the point that has the lowest f score
+    input: (list) open_list, (dict) g, (list) end
+    output: (list) best_point
+    '''
     best_point = open_list[0]
     best_f = g[best_point] + heuristic(best_point, end)
 
@@ -42,6 +62,13 @@ def get_lowest_f(open_list, g, end):
     return best_point
 
 def astar(grid, start, end):
+    '''
+    Purpose: Main function that loops for all the points until it reaches the ending point through while open_list:
+    Within this function, open_list stores all the points that had gone through, came_from is a dictionary that stores {neighbor: current}, 
+    g is a dictionary that stores the g_score for each point
+    input: (list) grid, (list) start, (list) end
+    output: (list) open_list
+    '''
     open_list = [start]
     came_from = {}
     g = {start: 0}
@@ -69,6 +96,11 @@ def astar(grid, start, end):
     return None
 
 def create_grid():
+    '''
+    Purpose: Creates the grid with the starting and the ending points
+    input: None
+    output: (list) grid, (list) start_coord, (list) end_coord
+    '''
     length = rand.randint(5, 30)
     width = rand.randint(5,30)
     grid = []
@@ -95,6 +127,11 @@ def create_grid():
     return grid, start_coord, end_coord
 
 def visualization(grid, path = None, start = None, end = None):
+    '''
+    Purpose: Shows a table of how the path is by showing the path as P, starting point as S, ending point as E, wall as #, empty spaces as *
+    input: (list) grid, (bool) path, (bool) start, (bool) end
+    output: (str) grid
+    '''
     for i in range(len(grid)):
         row_str = ""
         for j in range(len(grid[0])):
